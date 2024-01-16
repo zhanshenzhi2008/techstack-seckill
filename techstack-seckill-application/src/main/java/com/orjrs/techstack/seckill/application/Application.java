fc0c2a4d2e41ebb47d6c5f9c0f9ee71c65c78203
liujun <jun.liu_southchina@convertlab.com>
2024-01-16 19:50:24 +0800

    factor add  DDD construct

--- techstack-seckill-application/src/main/java/com/orjrs/techstack/seckill/application/Application.java
+++ techstack-seckill-application/src/main/java/com/orjrs/techstack/seckill/application/Application.java
@@ -3,6 +3,10 @@ package com.orjrs.techstack.seckill.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动应用层
+ * @author orjrs
+ */
 @SpringBootApplication
 public class Application {
 
